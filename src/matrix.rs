95e67d222c0787b53d32d4fac0709b9f2a90490b
Amirabbas Fazelinia <utilyre@proton.me>
2023-06-05 20:37:04 +0330

    feat(matrix): implement `iter_row` and `iter_col` for `Matrix`

--- src/matrix.rs
+++ src/matrix.rs
@@ -1,5 +1,6 @@
 use std::ops::{Add, AddAssign};
 
+// TODO: implement Debug manually
 #[derive(Debug, Eq)]
 pub struct Matrix<T, const ROWS: usize, const COLS: usize> {
     entries: Vec<T>,
@@ -35,6 +36,14 @@ impl<T, const ROWS: usize, const COLS: usize> Matrix<T, ROWS, COLS> {
 
     pub fn get(&self, i: usize, j: usize) -> Option<&T> {
         self.entries.get(COLS * i + j)
+    }
+
+    pub fn iter_row(&self, i: usize) -> Option<RowIter<T, COLS>> {
+        (i < ROWS).then_some(RowIter::new(&self.entries, i))
+    }
+
+    pub fn iter_col(&self, j: usize) -> Option<ColIter<T, ROWS, COLS>> {
+        (j < COLS).then_some(ColIter::new(&self.entries, j))
     }
 }
 
@@ -81,6 +90,64 @@ where
     }
 }
 
+pub struct RowIter<'a, T, const COLS: usize> {
+    entries: &'a [T],
+    index: usize,
+    end: usize,
+}
+
+impl<'a, T, const COLS: usize> RowIter<'a, T, COLS> {
+    fn new(entries: &'a [T], i: usize) -> Self {
+        Self {
+            entries,
+            index: COLS * i,
+            end: COLS * (i + 1) - 1,
+        }
+    }
+}
+
+impl<'a, T, const COLS: usize> Iterator for RowIter<'a, T, COLS> {
+    type Item = &'a T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        (self.index <= self.end).then(|| {
+            let idx = self.index;
+            self.index += 1;
+
+            &self.entries[idx]
+        })
+    }
+}
+
+pub struct ColIter<'a, T, const ROWS: usize, const COLS: usize> {
+    entries: &'a [T],
+    index: usize,
+    end: usize,
+}
+
+impl<'a, T, const ROWS: usize, const COLS: usize> ColIter<'a, T, ROWS, COLS> {
+    fn new(entries: &'a [T], j: usize) -> Self {
+        Self {
+            entries,
+            index: j,
+            end: (ROWS - 1) * COLS + j,
+        }
+    }
+}
+
+impl<'a, T, const ROWS: usize, const COLS: usize> Iterator for ColIter<'a, T, ROWS, COLS> {
+    type Item = &'a T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        (self.index <= self.end).then(|| {
+            let idx = self.index;
+            self.index += COLS;
+
+            &self.entries[idx]
+        })
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -106,5 +173,43 @@ mod tests {
         let expected = Matrix::new(vec![10i32; 21]);
         m1 += m2;
         assert_eq!(expected, m1);
+    }
+
+    #[test]
+    fn iter_row() {
+        let m: Matrix<_, 5, 3> = Matrix::new(
+            vec![0u32; 15]
+                .into_iter()
+                .enumerate()
+                .map(|(i, _)| i + 1)
+                .collect(),
+        );
+
+        let mut iter = m.iter_row(2).unwrap();
+        assert_eq!(iter.next(), Some(&7));
+        assert_eq!(iter.next(), Some(&8));
+        assert_eq!(iter.next(), Some(&9));
+        assert_eq!(iter.next(), None);
+        assert_eq!(iter.next(), None);
+    }
+
+    #[test]
+    fn iter_col() {
+        let m: Matrix<_, 5, 3> = Matrix::new(
+            vec![0u32; 15]
+                .into_iter()
+                .enumerate()
+                .map(|(i, _)| i + 1)
+                .collect(),
+        );
+
+        let mut iter = m.iter_col(1).unwrap();
+        assert_eq!(iter.next(), Some(&2));
+        assert_eq!(iter.next(), Some(&5));
+        assert_eq!(iter.next(), Some(&8));
+        assert_eq!(iter.next(), Some(&11));
+        assert_eq!(iter.next(), Some(&14));
+        assert_eq!(iter.next(), None);
+        assert_eq!(iter.next(), None);
     }
 }
