4dab71de5382dfb9064ae1b11e34c9f5fcd3bf0d
Amirabbas Fazelinia <utilyre@proton.me>
2023-06-06 23:33:56 +0330

    feat(matrix): implement `Display` for `Matrix`

--- src/matrix.rs
+++ src/matrix.rs
@@ -3,6 +3,7 @@ pub mod ops;
 
 use crate::error::{Error, Result};
 use iter::{ColIter, RowIter};
+use std::fmt::{self, Display, Formatter};
 
 #[derive(Debug)]
 pub struct Matrix<T, const M: usize, const N: usize> {
@@ -48,6 +49,36 @@ impl<T, const M: usize, const N: usize> Matrix<T, M, N> {
 
     pub fn iter_col(&self, j: usize) -> Option<ColIter<T, M, N>> {
         (j < N).then_some(ColIter::new(&self.entries, j))
+    }
+}
+
+impl<T, const M: usize, const N: usize> Display for Matrix<T, M, N>
+where
+    T: Display,
+{
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        let entries: Vec<_> = self.entries.iter().map(|entry| entry.to_string()).collect();
+        let Some(longest) = entries.iter().map(|entry| entry.len()).max() else {
+            return write!(f, "||");
+        };
+
+        for i in 0..M {
+            write!(
+                f,
+                "|{}|",
+                &entries[N * i..N * (i + 1)]
+                    .iter()
+                    .fold(String::new(), |acc, entry| {
+                        acc + &format!("{: ^longest$}", entry, longest = longest + 2)
+                    })
+            )?;
+
+            if i < M - 1 {
+                writeln!(f)?;
+            }
+        }
+
+        Ok(())
     }
 }
 
