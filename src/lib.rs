d33139fba32a1fb7d241c4308857d108e64be403
Amirabbas Fazelinia <utilyre@proton.me>
2023-07-11 20:40:03 +0330

    feat(prelude): re-export all entities from module prelude

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 #![feature(array_methods)]
 
+pub mod prelude;
+
 pub mod identity;
 pub mod matrix;
 pub mod vec2;
---
+++ src/prelude.rs
@@ -0,0 +1,3 @@
+pub use crate::identity::*;
+pub use crate::matrix::*;
+pub use crate::vec2::*;
