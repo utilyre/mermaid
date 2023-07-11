3986034dd4b1f67e0eb4ab327f4eb5b2ae231ffb
Amirabbas Fazelinia <utilyre@proton.me>
2023-07-11 17:21:21 +0330

    feat(vec2): create simple `Vec2` struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,5 @@
 #![feature(array_methods)]
 
 pub mod identity;
-
 pub mod matrix;
-pub use matrix::Matrix;
+pub mod vec2;
---
+++ src/vec2.rs
@@ -0,0 +1,48 @@
+use std::ops::Mul;
+
+pub struct Vec2 {
+    pub x: f32,
+    pub y: f32,
+}
+
+impl Vec2 {
+    pub fn new(x: f32, y: f32) -> Self {
+        Self { x, y }
+    }
+
+    pub fn length(&self) -> f32 {
+        (self.x.powi(2) + self.y.powi(2)).sqrt()
+    }
+
+    pub fn dot(&self, rhs: Self) -> f32 {
+        self.x * rhs.x + self.y * rhs.y
+    }
+}
+
+impl From<(f32, f32)> for Vec2 {
+    fn from(value: (f32, f32)) -> Self {
+        Vec2::new(value.0, value.1)
+    }
+}
+
+impl From<[f32; 2]> for Vec2 {
+    fn from(value: [f32; 2]) -> Self {
+        Vec2::new(value[0], value[1])
+    }
+}
+
+impl Mul<f32> for Vec2 {
+    type Output = Self;
+
+    fn mul(self, rhs: f32) -> Self::Output {
+        Vec2::new(self.x * rhs, self.y * rhs)
+    }
+}
+
+impl Mul<Vec2> for f32 {
+    type Output = Vec2;
+
+    fn mul(self, rhs: Vec2) -> Self::Output {
+        Vec2::new(self * rhs.x, self * rhs.y)
+    }
+}
