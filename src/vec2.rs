a052848d57d6ce0446edd647494a3a9e585d9e94
Amirabbas Fazelinia <utilyre@proton.me>
2023-07-11 20:15:22 +0330

    feat(vec2): implement all arithmetic operations

--- src/vec2.rs
+++ src/vec2.rs
@@ -1,5 +1,5 @@
 use crate::matrix::Matrix;
-use std::ops::Mul;
+use std::ops::{Add, AddAssign, Mul, MulAssign, Neg, Sub, SubAssign};
 
 pub struct Vec2 {
     pub x: f32,
@@ -22,22 +22,60 @@ impl Vec2 {
 
 impl From<(f32, f32)> for Vec2 {
     fn from(value: (f32, f32)) -> Self {
-        Vec2::new(value.0, value.1)
+        Self::new(value.0, value.1)
     }
 }
 
 impl From<[f32; 2]> for Vec2 {
     fn from(value: [f32; 2]) -> Self {
-        Vec2::new(value[0], value[1])
+        Self::new(value[0], value[1])
     }
 }
 
 impl From<Matrix<f32, 2, 1>> for Vec2 {
     fn from(value: Matrix<f32, 2, 1>) -> Self {
-        Vec2::new(
+        Self::new(
             *value.get(0, 0).expect("is not out of bounds"),
             *value.get(1, 0).expect("is not out of bounds"),
         )
+    }
+}
+
+impl Add for Vec2 {
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        Self::new(self.x + rhs.x, self.y + rhs.y)
+    }
+}
+
+impl AddAssign for Vec2 {
+    fn add_assign(&mut self, rhs: Self) {
+        self.x += rhs.x;
+        self.y += rhs.y;
+    }
+}
+
+impl Sub for Vec2 {
+    type Output = Self;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        Self::new(self.x - rhs.x, self.y - rhs.y)
+    }
+}
+
+impl SubAssign for Vec2 {
+    fn sub_assign(&mut self, rhs: Self) {
+        self.x -= rhs.x;
+        self.y -= rhs.y;
+    }
+}
+
+impl Neg for Vec2 {
+    type Output = Self;
+
+    fn neg(self) -> Self::Output {
+        Self::new(-self.x, -self.y)
     }
 }
 
@@ -45,7 +83,14 @@ impl Mul<f32> for Vec2 {
     type Output = Self;
 
     fn mul(self, rhs: f32) -> Self::Output {
-        Vec2::new(self.x * rhs, self.y * rhs)
+        Self::new(self.x * rhs, self.y * rhs)
+    }
+}
+
+impl MulAssign<f32> for Vec2 {
+    fn mul_assign(&mut self, rhs: f32) {
+        self.x *= rhs;
+        self.y *= rhs;
     }
 }
 
