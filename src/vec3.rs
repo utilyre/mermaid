79249c5e6a965be162f4966c93f2ace63bbfd1ee
Amirabbas Fazelinia <utilyre@proton.me>
2023-07-12 00:09:42 +0330

    test(vec3): add unit test for `cross`

--- src/vec3.rs
+++ src/vec3.rs
@@ -161,6 +161,14 @@ mod tests {
     }
 
     #[test]
+    fn cross() {
+        let v1 = Vec3::new(1.0, 6.0, 3.0);
+        let v2 = Vec3::new(8.0, 9.0, 2.0);
+
+        assert_eq!(Vec3::new(-15.0, 22.0, -39.0), v1.cross(v2));
+    }
+
+    #[test]
     fn add() {
         let v1 = Vec3::new(23.0, 18.0, 1.0);
         let v2 = Vec3::new(5.0, 32.0, 12.0);
