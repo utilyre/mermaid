8f4c8b7e545af9609a0cb26a8fb323930771578d
Amirabbas Fazelinia <utilyre@proton.me>
2023-07-12 16:19:46 +0330

    refactor(matrix): move type aliases to their own sub module

---
+++ src/matrix/aliases.rs
@@ -0,0 +1,31 @@
+use super::Matrix;
+
+pub type Matrix1x1<T> = Matrix<T, 1, 1>;
+pub type Matrix1x2<T> = Matrix<T, 1, 2>;
+pub type Matrix1x3<T> = Matrix<T, 1, 3>;
+pub type Matrix1x4<T> = Matrix<T, 1, 4>;
+pub type Matrix1x5<T> = Matrix<T, 1, 5>;
+
+pub type Matrix2x1<T> = Matrix<T, 2, 1>;
+pub type Matrix2x2<T> = Matrix<T, 2, 2>;
+pub type Matrix2x3<T> = Matrix<T, 2, 3>;
+pub type Matrix2x4<T> = Matrix<T, 2, 4>;
+pub type Matrix2x5<T> = Matrix<T, 2, 5>;
+
+pub type Matrix3x1<T> = Matrix<T, 3, 1>;
+pub type Matrix3x2<T> = Matrix<T, 3, 2>;
+pub type Matrix3x3<T> = Matrix<T, 3, 3>;
+pub type Matrix3x4<T> = Matrix<T, 3, 4>;
+pub type Matrix3x5<T> = Matrix<T, 3, 5>;
+
+pub type Matrix4x1<T> = Matrix<T, 4, 1>;
+pub type Matrix4x2<T> = Matrix<T, 4, 2>;
+pub type Matrix4x3<T> = Matrix<T, 4, 3>;
+pub type Matrix4x4<T> = Matrix<T, 4, 4>;
+pub type Matrix4x5<T> = Matrix<T, 4, 5>;
+
+pub type Matrix5x1<T> = Matrix<T, 5, 1>;
+pub type Matrix5x2<T> = Matrix<T, 5, 2>;
+pub type Matrix5x3<T> = Matrix<T, 5, 3>;
+pub type Matrix5x4<T> = Matrix<T, 5, 4>;
+pub type Matrix5x5<T> = Matrix<T, 5, 5>;
