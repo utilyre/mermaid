113431dbc6a0f0fd3cb4270ba02519044907db1c
Amirabbas Fazelinia <utilyre@proton.me>
2023-07-12 17:19:19 +0330

    build(identity): improve macro

--- src/identity.rs
+++ src/identity.rs
@@ -2,52 +2,98 @@ pub trait IdAdd {
     fn id_add() -> Self;
 }
 
-macro_rules! impl_id_add {
-    ($t: ty, $v: expr) => {
-        impl IdAdd for $t {
-            fn id_add() -> Self {
-                $v
-            }
-        }
-    };
-}
-
-impl_id_add!(u8, 0);
-impl_id_add!(u16, 0);
-impl_id_add!(u32, 0);
-impl_id_add!(u64, 0);
-impl_id_add!(usize, 0);
-impl_id_add!(i8, 0);
-impl_id_add!(i16, 0);
-impl_id_add!(i32, 0);
-impl_id_add!(i64, 0);
-impl_id_add!(isize, 0);
-impl_id_add!(f32, 0.0);
-impl_id_add!(f64, 0.0);
-
 pub trait IdMul {
     fn id_mul() -> Self;
 }
 
-macro_rules! impl_id_mul {
-    ($t: ty, $v: expr) => {
-        impl IdMul for $t {
+macro_rules! id_impl {
+    (
+        Self = $Self:ty,
+        id_add = $id_add:literal,
+        id_mul = $id_mul:literal,
+    ) => {
+        impl IdAdd for $Self {
+            fn id_add() -> Self {
+                $id_add
+            }
+        }
+
+        impl IdMul for $Self {
             fn id_mul() -> Self {
-                $v
+                $id_mul
             }
         }
     };
 }
 
-impl_id_mul!(u8, 1);
-impl_id_mul!(u16, 1);
-impl_id_mul!(u32, 1);
-impl_id_mul!(u64, 1);
-impl_id_mul!(usize, 1);
-impl_id_mul!(i8, 1);
-impl_id_mul!(i16, 1);
-impl_id_mul!(i32, 1);
-impl_id_mul!(i64, 1);
-impl_id_mul!(isize, 1);
-impl_id_mul!(f32, 1.0);
-impl_id_mul!(f64, 1.0);
+id_impl! {
+    Self = u8,
+    id_add = 0,
+    id_mul = 1,
+}
+
+id_impl! {
+    Self = u16,
+    id_add = 0,
+    id_mul = 1,
+}
+
+id_impl! {
+    Self = u32,
+    id_add = 0,
+    id_mul = 1,
+}
+
+id_impl! {
+    Self = u64,
+    id_add = 0,
+    id_mul = 1,
+}
+
+id_impl! {
+    Self = usize,
+    id_add = 0,
+    id_mul = 1,
+}
+
+id_impl! {
+    Self = i8,
+    id_add = 0,
+    id_mul = 1,
+}
+
+id_impl! {
+    Self = i16,
+    id_add = 0,
+    id_mul = 1,
+}
+
+id_impl! {
+    Self = i32,
+    id_add = 0,
+    id_mul = 1,
+}
+
+id_impl! {
+    Self = i64,
+    id_add = 0,
+    id_mul = 1,
+}
+
+id_impl! {
+    Self = isize,
+    id_add = 0,
+    id_mul = 1,
+}
+
+id_impl! {
+    Self = f32,
+    id_add = 0.0,
+    id_mul = 1.0,
+}
+
+id_impl! {
+    Self = f64,
+    id_add = 0.0,
+    id_mul = 1.0,
+}
