989a376cd989cd4b6a6303f13a59cbe98e647d61
Amirabbas Fazelinia <utilyre@proton.me>
2023-07-12 18:30:15 +0330

    feat(matrix): add type aliases until 9x9

--- src/matrix/aliases.rs
+++ src/matrix/aliases.rs
@@ -5,27 +5,87 @@ pub type Matrix1x2<T> = Matrix<T, 1, 2>;
 pub type Matrix1x3<T> = Matrix<T, 1, 3>;
 pub type Matrix1x4<T> = Matrix<T, 1, 4>;
 pub type Matrix1x5<T> = Matrix<T, 1, 5>;
+pub type Matrix1x6<T> = Matrix<T, 1, 5>;
+pub type Matrix1x7<T> = Matrix<T, 1, 5>;
+pub type Matrix1x8<T> = Matrix<T, 1, 5>;
+pub type Matrix1x9<T> = Matrix<T, 1, 5>;
 
 pub type Matrix2x1<T> = Matrix<T, 2, 1>;
 pub type Matrix2x2<T> = Matrix<T, 2, 2>;
 pub type Matrix2x3<T> = Matrix<T, 2, 3>;
 pub type Matrix2x4<T> = Matrix<T, 2, 4>;
 pub type Matrix2x5<T> = Matrix<T, 2, 5>;
+pub type Matrix2x6<T> = Matrix<T, 1, 5>;
+pub type Matrix2x7<T> = Matrix<T, 1, 5>;
+pub type Matrix2x8<T> = Matrix<T, 1, 5>;
+pub type Matrix2x9<T> = Matrix<T, 1, 5>;
 
 pub type Matrix3x1<T> = Matrix<T, 3, 1>;
 pub type Matrix3x2<T> = Matrix<T, 3, 2>;
 pub type Matrix3x3<T> = Matrix<T, 3, 3>;
 pub type Matrix3x4<T> = Matrix<T, 3, 4>;
 pub type Matrix3x5<T> = Matrix<T, 3, 5>;
+pub type Matrix3x6<T> = Matrix<T, 1, 5>;
+pub type Matrix3x7<T> = Matrix<T, 1, 5>;
+pub type Matrix3x8<T> = Matrix<T, 1, 5>;
+pub type Matrix3x9<T> = Matrix<T, 1, 5>;
 
 pub type Matrix4x1<T> = Matrix<T, 4, 1>;
 pub type Matrix4x2<T> = Matrix<T, 4, 2>;
 pub type Matrix4x3<T> = Matrix<T, 4, 3>;
 pub type Matrix4x4<T> = Matrix<T, 4, 4>;
 pub type Matrix4x5<T> = Matrix<T, 4, 5>;
+pub type Matrix4x6<T> = Matrix<T, 1, 5>;
+pub type Matrix4x7<T> = Matrix<T, 1, 5>;
+pub type Matrix4x8<T> = Matrix<T, 1, 5>;
+pub type Matrix4x9<T> = Matrix<T, 1, 5>;
 
 pub type Matrix5x1<T> = Matrix<T, 5, 1>;
 pub type Matrix5x2<T> = Matrix<T, 5, 2>;
 pub type Matrix5x3<T> = Matrix<T, 5, 3>;
 pub type Matrix5x4<T> = Matrix<T, 5, 4>;
 pub type Matrix5x5<T> = Matrix<T, 5, 5>;
+pub type Matrix5x6<T> = Matrix<T, 1, 5>;
+pub type Matrix5x7<T> = Matrix<T, 1, 5>;
+pub type Matrix5x8<T> = Matrix<T, 1, 5>;
+pub type Matrix5x9<T> = Matrix<T, 1, 5>;
+
+pub type Matrix6x1<T> = Matrix<T, 5, 1>;
+pub type Matrix6x2<T> = Matrix<T, 5, 2>;
+pub type Matrix6x3<T> = Matrix<T, 5, 3>;
+pub type Matrix6x4<T> = Matrix<T, 5, 4>;
+pub type Matrix6x5<T> = Matrix<T, 5, 5>;
+pub type Matrix6x6<T> = Matrix<T, 1, 5>;
+pub type Matrix6x7<T> = Matrix<T, 1, 5>;
+pub type Matrix6x8<T> = Matrix<T, 1, 5>;
+pub type Matrix6x9<T> = Matrix<T, 1, 5>;
+
+pub type Matrix7x1<T> = Matrix<T, 5, 1>;
+pub type Matrix7x2<T> = Matrix<T, 5, 2>;
+pub type Matrix7x3<T> = Matrix<T, 5, 3>;
+pub type Matrix7x4<T> = Matrix<T, 5, 4>;
+pub type Matrix7x5<T> = Matrix<T, 5, 5>;
+pub type Matrix7x6<T> = Matrix<T, 1, 5>;
+pub type Matrix7x7<T> = Matrix<T, 1, 5>;
+pub type Matrix7x8<T> = Matrix<T, 1, 5>;
+pub type Matrix7x9<T> = Matrix<T, 1, 5>;
+
+pub type Matrix8x1<T> = Matrix<T, 5, 1>;
+pub type Matrix8x2<T> = Matrix<T, 5, 2>;
+pub type Matrix8x3<T> = Matrix<T, 5, 3>;
+pub type Matrix8x4<T> = Matrix<T, 5, 4>;
+pub type Matrix8x5<T> = Matrix<T, 5, 5>;
+pub type Matrix8x6<T> = Matrix<T, 1, 5>;
+pub type Matrix8x7<T> = Matrix<T, 1, 5>;
+pub type Matrix8x8<T> = Matrix<T, 1, 5>;
+pub type Matrix8x9<T> = Matrix<T, 1, 5>;
+
+pub type Matrix9x1<T> = Matrix<T, 5, 1>;
+pub type Matrix9x2<T> = Matrix<T, 5, 2>;
+pub type Matrix9x3<T> = Matrix<T, 5, 3>;
+pub type Matrix9x4<T> = Matrix<T, 5, 4>;
+pub type Matrix9x5<T> = Matrix<T, 5, 5>;
+pub type Matrix9x6<T> = Matrix<T, 1, 5>;
+pub type Matrix9x7<T> = Matrix<T, 1, 5>;
+pub type Matrix9x8<T> = Matrix<T, 1, 5>;
+pub type Matrix9x9<T> = Matrix<T, 1, 5>;
