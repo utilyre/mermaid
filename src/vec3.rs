fa3600f8e89897c517d8119621c20b272bf6c8d5
Amirabbas Fazelinia <utilyre@proton.me>
2023-07-14 11:36:00 +0330

    refactor(vec3): bring method `cross` next to method `dot`

--- src/vec3.rs
+++ src/vec3.rs
@@ -29,16 +29,16 @@ impl Vec3 {
         self.x * rhs.x + self.y * rhs.y + self.z * rhs.z
     }
 
-    pub fn proj(self, base: Self) -> Self {
-        (self.dot(base) / base.len().powi(2)) * base
-    }
-
     pub fn cross(self, rhs: Self) -> Self {
         Self::new(
             self.y * rhs.z - self.z * rhs.y,
             self.z * rhs.x - self.x * rhs.z,
             self.x * rhs.y - self.y * rhs.x,
         )
+    }
+
+    pub fn proj(self, base: Self) -> Self {
+        (self.dot(base) / base.len().powi(2)) * base
     }
 }
 
