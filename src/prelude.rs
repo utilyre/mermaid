fbb47cc2317d06bdf197f52d96c17377e67b2511
Amirabbas Fazelinia <utilyre@proton.me>
2023-07-15 13:40:35 +0330

    chore(prelude): re-export `CursorIterator`

--- src/prelude.rs
+++ src/prelude.rs
@@ -1,5 +1,6 @@
 pub use crate::identity::*;
 pub use crate::matrix::aliases::*;
+pub use crate::matrix::iter::cursor::CursorIterator;
 pub use crate::matrix::*;
 pub use crate::vec2::*;
 pub use crate::vec3::*;
